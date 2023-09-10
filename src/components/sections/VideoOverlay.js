3ffe0617201f6587b69b807ebcd498748575a94f
unknown <benjoniksic@gmail.com>
2023-09-10 22:21:50 +0200

    Iphone video fix

--- src/components/sections/VideoOverlay.js
+++ src/components/sections/VideoOverlay.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import Hero from "./Hero";
 import videoBgCompressed from "../../assets/videos/videoBgCompressed.mp4";
 
 const VideoOverlay = () => {
+    const videoElement = useRef(null)
+    const touchStartHandler = () => {
+        videoElement.current.play()
+    }
+    
     return (
         <>
-            <div className="video-container">
+            <div className="video-container" onTouchStart={touchStartHandler}>
                 <div className="video-overlay" />
-                <video src={videoBgCompressed} autoPlay loop muted />
+                <video src={videoBgCompressed} autoPlay loop muted ref={videoElement}/>
                 <div className="video-content">
                     <Hero className="illustration-section-01" />
                 </div>
