385cd43e73a2292be1c68020f5c56f504c102c4f
unknown <benjoniksic@gmail.com>
2023-09-18 23:54:17 +0200

    Revert "Low battery video fix"
    This reverts commit 13206c89ca5c7c66029260018bb35c573b4cbda4.

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -11,7 +11,7 @@ const FooterNav = ({ className, ...props }) => {
 
     useEffect(() => {
         scrollToTop();
-    }, [])
+    })
 
     return (
         <nav {...props} className={classes}>
--- src/components/sections/VideoOverlay.js
+++ src/components/sections/VideoOverlay.js
@@ -1,22 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Hero from "./Hero";
 import videoBgCompressed from "../../assets/videos/videoBgCompressed.mp4";
 
 const VideoOverlay = () => {
-    useEffect(() => {
-        const videoElement = document.getElementById("home_video");
-      if (videoElement?.paused === true) {
-          videoElement.play();
-      }
-    }, []);
-
     return (
         <>
             <div className="video-container">
                 <div className="video-overlay" />
                 <div 
                     dangerouslySetInnerHTML={
-                        { __html: `<video id="home_video" loop muted autoplay playsinline src="${videoBgCompressed}" style="object-fit: cover;"/>` }
+                        { __html: `<video loop muted autoplay playsinline src="${videoBgCompressed}" style="object-fit: cover;"/>` }
                     }
                     className="video-class"
                 >
